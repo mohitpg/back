6bbb210a6f2a9f24182461f3d96fd64cc97e3c67
Mohitpg <mohitp.gupta1102@gmail.com>
2023-07-25 23:49:36 +0530

    Updated Requirements and added complete model code

--- backend/model.py
+++ backend/model.py
@@ -13,14 +13,15 @@ import json
 from time import time
 import cv2
 import pickle
-# from keras.applications.vgg16 import VGG16
-# from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
-# from keras.preprocessing import image
-# from keras.models import Model, load_model
-# from keras.preprocessing.sequence import pad_sequences
-# from keras.utils import to_categorical
-# from keras.layers import Input, Dense, Dropout, Embedding, LSTM
-#from keras.layers.merge import add
+from keras.applications.vgg16 import VGG16
+from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
+from keras.preprocessing import image
+from keras.models import Model, load_model
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.layers import Input, Dense, Dropout, Embedding, LSTM, add
+
+#Reading the Captions
 def readTextFile(path):
     with open(path) as f:
         captions = f.read()
@@ -29,6 +30,7 @@ captions  = readTextFile("./Data/Flickr8k.token.txt")
 captions = captions.split('\n')
 print(len(captions))
 
+# Dictionary to Map each Image with the list of captions it has
 descriptions = {}
 for x in captions:
     first,second = x.split('\t')
@@ -37,6 +39,15 @@ for x in captions:
         descriptions[img_name] = []
     descriptions[img_name].append(second)
 
+#View Images
+img = cv2.imread("Data/1000268201_693b08cb0e.jpg")
+img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+plt.imshow(img)
+plt.axis("off")
+plt.show()
+
+
+#Data Cleaning
 def clean_text(sentence):
     sentence = sentence.lower()
     sentence = re.sub("[^a-z]+"," ",sentence)
@@ -53,6 +64,8 @@ for key,caption_list in descriptions.items():
 with open("descriptions_1.txt","w") as f:
     f.write(str(descriptions))
 
+
+#Making Vocabulary
 descriptions = None
 with open("descriptions_1.txt",'r') as f:
     descriptions= f.read()
@@ -71,14 +84,68 @@ for key in descriptions.keys():
 counter = collections.Counter(total_words)
 freq_cnt = dict(counter)
 print(len(freq_cnt.keys()))
-# Sort this dictionary according to the freq count
 sorted_freq_cnt = sorted(freq_cnt.items(),reverse=True,key=lambda x:x[1])
-# Filter
 threshold = 10
 sorted_freq_cnt  = [x for x in sorted_freq_cnt if x[1]>threshold]
 total_words = [x[0] for x in sorted_freq_cnt]
 print(len(total_words))
 
+#Prepare Training and testing data
+train_file_data = readTextFile("Data/Flickr_TextData/Flickr_8k.trainImages.txt")
+test_file_data = readTextFile("Data/Flickr_TextData/Flickr_8k.testImages.txt")
+train = [row.split(".")[0] for row in train_file_data.split("\n")[:-1]]
+test = [row.split(".")[0] for row in test_file_data.split("\n")[:-1]]
+
+train_descriptions = {}
+
+for img_id in train:
+    train_descriptions[img_id] = []
+    for cap in descriptions[img_id]:
+        cap_to_append = "startseq "  + cap + " endseq"
+        train_descriptions[img_id].append(cap_to_append)
+
+#Image Feature Extraction
+model = ResNet50(weights="imagenet",input_shape=(224,224,3))
+model_new = Model(model.input,model.layers[-2].output)
+def preprocess_img(img):
+    img = image.load_img(img,target_size=(224,224))
+    img = image.img_to_array(img)
+    img = np.expand_dims(img,axis=0)
+    img = preprocess_input(img)
+    return img
+def encode_image(img):
+    img = preprocess_img(img)
+    feature_vector = model_new.predict(img)
+    
+    feature_vector = feature_vector.reshape((-1,))
+    return feature_vector
+#Save training data
+start = time()
+encoding_train = {}
+for ix,img_id in enumerate(train):
+    img_path = "Data"+"/"+img_id+".jpg"
+    encoding_train[img_id] = encode_image(img_path)
+    if ix%100==0:
+        print("Encoding in Progress Time step %d "%ix)       
+end_t = time()
+print("Total Time Taken :",end_t-start)
+with open("saved/encoded_train_features.pkl","wb") as f:
+    pickle.dump(encoding_train,f)
+
+#Save test data
+start = time()
+encoding_test = {}
+for ix,img_id in enumerate(test):
+    img_path = "Data"+"/"+img_id+".jpg"
+    encoding_test[img_id] = encode_image(img_path)
+    if ix%100==0:
+        print("Test Encoding in Progress Time step %d "%ix)    
+end_t = time()
+print("Total Time Taken(test) :",end_t-start)
+with open("saved/encoded_test_features.pkl","wb") as f:
+    pickle.dump(encoding_test,f)
+
+#Caption feature extraction and preprocessing
 word_to_idx = {}
 idx_to_word = {}
 for i,word in enumerate(total_words):
@@ -101,3 +168,86 @@ with open('saved_word_to_idx.pkl', 'wb') as f:
 with open('saved_idx_to_wordy.pkl', 'wb') as f:
     pickle.dump(idx_to_word, f)
     
+#Create data generator
+def data_generator(train_descriptions,encoding_train,word_to_idx,max_len,batch_size):
+    X1,X2, y = [],[],[]
+    n =0
+    while True:
+        for key,desc_list in train_descriptions.items():
+            n += 1
+            
+            photo = encoding_train[key+".jpg"]
+            for desc in desc_list:
+                
+                seq = [word_to_idx[word] for word in desc.split() if word in word_to_idx]
+                for i in range(1,len(seq)):
+                    xi = seq[0:i]
+                    yi = seq[i]
+                    
+                    #0 denote padding word
+                    xi = pad_sequences([xi],maxlen=max_len,value=0,padding='post')[0]
+                    yi = to_categorical([yi],num_classes=vocab_size)[0]
+                    
+                    X1.append(photo)
+                    X2.append(xi)
+                    y.append(yi)
+                    
+                if n==batch_size:
+                    yield [[np.array(X1),np.array(X2)],np.array(y)]
+                    X1,X2,y = [],[],[]
+                    n = 0
+
+#Word Embeddings
+with open("./saved/glove.6B.50d.txt",encoding='utf8') as f:
+    embedding_index = {}
+    for line in f:
+        values = line.split()
+        word = values[0]
+        word_embedding = np.array(values[1:],dtype='float')
+        embedding_index[word] = word_embedding
+
+def get_embedding_matrix():
+    emb_dim = 50
+    matrix = np.zeros((vocab_size,emb_dim))
+    for word,idx in word_to_idx.items():
+        embedding_vector = embedding_index.get(word)
+        
+        if embedding_vector is not None:
+            matrix[idx] = embedding_vector
+            
+    return matrix
+embedding_matrix = get_embedding_matrix()
+
+#Main model
+input_img_features = Input(shape=(2048,))
+inp_img1 = Dropout(0.3)(input_img_features)
+inp_img2 = Dense(256,activation='relu')(inp_img1)
+
+input_captions = Input(shape=(max_len,))
+inp_cap1 = Embedding(input_dim=vocab_size,output_dim=50,mask_zero=True)(input_captions)
+inp_cap2 = Dropout(0.3)(inp_cap1)
+inp_cap3 = LSTM(256)(inp_cap2)
+
+decoder1 = add([inp_img2,inp_cap3])
+decoder2 = Dense(256,activation='relu')(decoder1)
+outputs = Dense(vocab_size,activation='softmax')(decoder2)
+
+# Combined Model
+model = Model(inputs=[input_img_features,input_captions],outputs=outputs)
+model.summary()
+model.layers[2].set_weights([embedding_matrix])
+model.layers[2].trainable = False
+
+model.compile(loss='categorical_crossentropy',optimizer="adam")
+
+#Training the Model
+epochs = 20
+batch_size = 3
+steps = len(train_descriptions)
+def train():
+    for i in range(epochs):
+        generator = data_generator(train_descriptions,encoding_train,word_to_idx,max_len,batch_size)
+        model.fit_generator(generator,epochs=1,steps_per_epoch=steps,verbose=1)
+        model.save('./model_weights/model_'+str(i)+'.h5')
+
+#----------------END---------------------
